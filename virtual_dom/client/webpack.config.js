2e8f4bf4667431040f5d0a9fbb13cef4545a721f
kingweicai <kingweicai@tencent.com>
2019-03-18 20:11:15 +0800

    feat: virtual dom

---
+++ virtual_dom/client/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, '../dist/js'),
+        filename: 'index.bundle.js'
+    }
+};
---
+++ virtual_dom/server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const httpConfig = {
+    port: 12347,
+    address: '127.0.0.1'
+};
+
+app.use('/static', express.static('dist'));
+
+app.listen(httpConfig.port, httpConfig.address, function (err) {
+    if(err){
+        console.error(err);
+        process.abort();
+    }else{
+        console.log(`Example app listening on ${httpConfig.address}:${httpConfig.port}!`);
+    }
+
+});
