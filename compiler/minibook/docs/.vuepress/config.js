d9fe1a8ef2ea6e7ae3c5c3558686d4b0b7e53a92
caijw <285025525@qq.com>
2019-07-07 17:33:30 +0800

    feat； update

--- compiler/minibook/docs/.vuepress/config.js
+++ compiler/minibook/docs/.vuepress/config.js
@@ -5,7 +5,28 @@ module.exports = {
   themeConfig: {
     navbar: false,
     sidebar: [
-      ['/01/whatIsCompiler', '什么是编译']
+      {
+        title: '绪论',
+        collapsable: true,
+        children: [
+          ['/01/01_whatIsCompiler', '什么是编译'],
+          ['/01/02_compilerSystem', '编译系统的结构'],
+          ['/01/03_lexicalAnalysis', '词法分析'],
+          ['/01/04_syntaxAnalysis.md', '语法分析'],
+          ['/01/05_semanticAnalysis.md', '语义分析'],
+          ['/01/06_midCode_backend.md', '中间代码生成和后端'],
+        ]
+      },
+      {
+        title: '程序设计语言及其文法',
+        collapsable: true,
+        children: [
+          ['/02/01_basic', '基本概念'],
+        ]
+      }
+
+      
+      
     ]
   }
 }
---
+++ git/gitLabFlow.md
@@ -0,0 +1,4 @@
+最大原则叫做"上游优先"（upsteam first），即只存在一个主分支master，它是所有其他分支的"上游"。只有上游分支采纳的代码变化，才能应用到其他分支。
+
+master -> pre-production -> production
+
