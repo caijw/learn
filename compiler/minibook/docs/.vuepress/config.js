ba8b19c9c353878623bdf877dbb904fb7e7dc886
caijw <285025525@qq.com>
2019-07-07 15:37:30 +0800

    feat: 编译原理

---
+++ compiler/minibook/docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: '编译原理',
+  description: '编译原理实践',
+  head: [],
+  themeConfig: {
+    navbar: false,
+    sidebar: [
+      ['/01/whatIsCompiler', '什么是编译']
+    ]
+  }
+}
