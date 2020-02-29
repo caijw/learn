24a2e727cfc5e9343fd1e975e05cd4984ad570ac
caijw <285025525@qq.com>
2020-03-01 00:14:27 +0800

    feat: update

--- dart/1.dart
+++ dart/1.dart
@@ -3,18 +3,21 @@ printInteger(int aNumber) {
   print('The number is $aNumber.'); // 打印到控制台。
 }
 
-class B  {
+class B {
   void fun1() {
     // fun();
   }
 }
 
-
-// mixin A 只能用在 B 上面
+// 只有某些类型可以mix in A，这些类型是B的子类
 mixin A on B {
   void fun() {
     fun1();
   }
+}
+
+class C with B, A {
+
 }
 
 // class C with A {
