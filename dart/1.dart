175cad11b3971e5c58f9d9a0b8631a6c46d6b5a1
caijw <285025525@qq.com>
2020-02-29 22:35:52 +0800

    feat: update

--- dart/1.dart
+++ dart/1.dart
@@ -2,11 +2,27 @@
 printInteger(int aNumber) {
   print('The number is $aNumber.'); // 打印到控制台。
 }
+// mixin A 只能用在 B 上面
+mixin A on B {
+  void fun() {
+    print('A fun');
+  }
+}
+
+class B {
+  void fun() {
+    print('B fun');
+  }
+}
+
+class C with A {
+  void fun1() {
+    fun();
+  }
+}
 
 // 应用从这里开始执行。
 main() {
-  var number = 42; // 声明并初始化一个变量。
-  printInteger(number); // 调用函数。
-  int lineCount;
-  assert(lineCount != null);
+  C c = C();
+  c.fun1();
 }
