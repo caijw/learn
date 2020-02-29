2b0ae97d6dd66c44b3341dc65f2d27d1bc8e4dbe
caijw <285025525@qq.com>
2020-02-29 23:42:25 +0800

    feat: update

--- dart/1.dart
+++ dart/1.dart
@@ -2,27 +2,28 @@
 printInteger(int aNumber) {
   print('The number is $aNumber.'); // 打印到控制台。
 }
+
+class B  {
+  void fun1() {
+    // fun();
+  }
+}
+
+
 // mixin A 只能用在 B 上面
 mixin A on B {
   void fun() {
-    print('A fun');
+    fun1();
   }
 }
 
-class B {
-  void fun() {
-    print('B fun');
-  }
-}
-
-class C with A {
-  void fun1() {
-    fun();
-  }
-}
+// class C with A {
+//   void fun1() {
+//     fun();
+//   }
+// }
 
 // 应用从这里开始执行。
 main() {
-  C c = C();
-  c.fun1();
+
 }
